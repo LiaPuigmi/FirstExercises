093d9e68b1824db586ce20eb99ef0aafd0f1ce78
LiaPuigmi <liapuigmi@gmail.com>
2023-11-02 12:14:14 +0100

    adding init init

---
+++ src/Program1.java
@@ -0,0 +1,20 @@
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//No funciona el emoji en consola jeje xd
+		System.out.println("I ♥ love Java");
+		System.out.println("Buenas tardes,\nmi nombre es Lia.");
+		System.out.println("Buenas tardes,\tmi nombre es Lia.");
+		System.out.println("Hola\\(o.o)/");
+		System.out.println("\"Buenas\" tardes...");
+		System.out.println("\'Malas\'? tardes");
+		System.out.println("Buenas tardes,\rmi nombre es Lia.");
+		System.out.println("Buenas tardes, ,\bmi nombre es Lia.");
+
+
+
+	}
+
+}
