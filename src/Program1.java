08a80397d813bafad260da65d0b8e6b3f27606db
LiaPuigmi <liapuigmi@gmail.com>
2023-11-03 12:32:47 +0100

    ejercicio calculo rangos

--- src/Program1.java
+++ src/Program1.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 
 public class Program1 {
 
@@ -17,6 +18,8 @@ public class Program1 {
 		HelloWorld.main(args);
 		HelloWorld.ejercicioTipo();
 		HelloWorld.ejercicioCircular();
+		HelloWorld.ejercicioConstantes();
+		HelloWorld.ejercicioCalculoRango();
 	}
 
 	//Classe con nombre HelloWorld que imprime la suma de dos numeros integers
@@ -76,6 +79,74 @@ public class HelloWorld{
 		long numLong=(long)(Long.MAX_VALUE+20000);
 		System.out.println("Long Circular 20000 posiciones: " + numLong);
 	}
+	
+	/* EJERCICIO TRANSFORMAR A CONSTANTES PRIMITIVOS */
+	public static void ejercicioConstantes() {
+		System.out.println("EJERCICIO TRANSFORMACIÓN PRIMITIVOS A CONSTANTES");
+		//Lógicos CONSTANTES
+		final boolean INTERRUPTOR=false;
+		System.out.println("Boolean: "+INTERRUPTOR);
+		//Carácter CONSTANTES
+		final char LETRA='A';
+		System.out.println("Char: "+LETRA);
+		//Numéricos enteros CONSTANTES
+		final byte NUMERITO=29;
+		System.out.println("Byte: "+NUMERITO);
+		final short NUMERITO_MAYOR=32000;
+		System.out.println("Short: "+NUMERITO_MAYOR);
+		final int NUMERO_STANDAR=320000;
+		System.out.println("Int: "+NUMERO_STANDAR);
+		final long NUMERO_MAYOR=3200000000L;
+		System.out.println("Long: "+NUMERO_MAYOR);
+		//Numéricos reales CONSTANTES
+		final float NUM_REAL_CHIKITO=22.333f;
+		System.out.println("Float: "+NUM_REAL_CHIKITO);
+		final double NUM_REAL_MAYOR=22.33333333333;
+		System.out.println("Double: "+NUM_REAL_MAYOR);
+		
+	}
+	
+	/* EJERCICIO CALCULAR RANGOS PRIMITIVOS */
+	/* Calcula el rango de todos los tipos de datos primitivos.
+		Veras que tanto char y boolean tienen solamente positivos. Y los números (al
+		tener necesidades distintas), se subdividen en valores positivos y negativos.
+		-> 2(base binaria)^X(bits)=Y(total del rango)
+		• Y(total del rango)/2=Z(valores para positivos y negativos)*/
+	
+	public static void ejercicioCalculoRango() {
+		int numBool= (int) Math.pow(2, 1);
+		System.out.println("Total rango boolean: "+numBool);
+		System.out.println("Valores negativos boolean: "+ numBool/2 +" y positivos: "+numBool/2);
+		
+		int numChar=(int) Math.pow(2, 16);
+		System.out.println("Total rango char: "+numChar);
+		System.out.println("Valores negativos boolean: 0 y positivos: "+(numChar-1));
+		
+		int numByte=(int) Math.pow(2, 8);
+		System.out.println("Total rango byte: "+numByte);
+		System.out.println("Valores negativos byte: "+-numByte/2 +" y positivos: "+(numByte/2-1));
+		
+		int numShort=(int) Math.pow(2, 16);
+		System.out.println("Total rango short: "+numShort);
+		System.out.println("Valores negativos short: "+ -numShort/2 +" y positivos: "+(numShort/2-1));
+		
+		long numInt=(long) Math.pow(2, 32);
+		System.out.println("Total rango int: "+numInt);
+		System.out.println("Valores negativos int: "+ -numInt/2 +" y positivos: "+(numInt/2-1));
+		
+        BigInteger numLong = new BigInteger("2").pow(64);
+		System.out.println("Total rango long: "+numLong);
+		System.out.println("Valores negativos long: "+ Long.MIN_VALUE +" y positivos: "+Long.MAX_VALUE);
+		
+		long numFloat=(long) Math.pow(2, 32);
+		System.out.println("Total rango float: "+numFloat);
+		System.out.println("Valores negativos float: "+ Float.MIN_VALUE +" y positivos: "+Float.MAX_VALUE);
+		
+        BigInteger numDouble = new BigInteger("2").pow(64);
+		System.out.println("Total rango double: "+numDouble);
+		System.out.println("Valores negativos double: "+ Double.MIN_VALUE +" y positivos: "+ Double.MAX_VALUE);
+	}
+	
 	
 }
 
